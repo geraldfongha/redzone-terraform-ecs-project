ba9d3c83c6f4e331d8e0591bdc79391e71886f26
gerald fongha <fitzgeraldh2k@gmail.com>
2023-03-14 21:44:10 +0000

    terraform files created

---
+++ backend.tf
@@ -0,0 +1,11 @@
+#store the terraform state file in s3 and lock with dynamodb
+terraform {
+  backend "s3" {
+    bucket       = "fongha-terraform-remote-state"
+    key          = "redzone-ecs/terraform.tfstate"
+    region       = "us-east-1"
+    profile      =  "terraform-user"
+    dynamodb_table = "terraform-state-lock"
+    
+  }
+}
